f3d7394f3d446aa4596d5c2dcd2a076c7c3f45ca
Jonas Silva <STRANG3R973@outlook.com>
2022-05-08 21:00:10 -0300

    "@nebular/firebase-auth" added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,45 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
 import { TodosListComponent } from './todos-list/todos-list.component';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+} from '@nebular/auth';
 
 const routes: Routes = [
   { path: 'todos-list', component: TodosListComponent },
-  { path: 'todo-details', component: TodoDetailsComponent },
-  { path: '', redirectTo: '/todos-list', pathMatch: 'full' },
+  {
+    path: 'todo-details',
+    component: TodoDetailsComponent,
+    canActivate: [AngularFireAuthGuard],
+  },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: 'todos-list', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,13 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { NbAuthModule, NbAuthService } from '@nebular/auth';
+import {
+  NbFirebaseAuthModule,
+  NbFirebasePasswordStrategy,
+} from '@nebular/firebase-auth';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [AppComponent, TodosListComponent, TodoDetailsComponent],
@@ -27,6 +34,7 @@ import { TodoDetailsComponent } from './todo-details/todo-details.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
     NbEvaIconsModule,
@@ -36,11 +44,47 @@ import { TodoDetailsComponent } from './todo-details/todo-details.component';
     NbCardModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    AngularFireAuthModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)), // TODO test it
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    NbFirebaseAuthModule,
+    NbAuthModule.forRoot({
+      strategies: [
+        NbFirebasePasswordStrategy.setup({
+          name: 'password',
+          requestPassword: {
+            redirect: {
+              success: null,
+            },
+          },
+        }),
+      ],
+      forms: {
+        login: {
+          strategy: 'password',
+          rememberMe: true,
+        },
+        logout: {
+          strategy: 'password',
+        },
+        requestPassword: {
+          strategy: 'password',
+        },
+        validation: {
+          password: {
+            required: true,
+            minLength: 6,
+            maxLength: 50,
+          },
+          email: {
+            required: true,
+          },
+        },
+      },
+    }),
   ],
-  providers: [],
+  providers: [NbAuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
